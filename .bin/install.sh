da3def87d00642945ee98d58265bc3cdb2428d94
Tomas Ruud <tomasruu@gmail.com>
2019-09-28 10:51:28 +0200

    Switching from whitelist to manual adding only

--- .bin/install.sh
+++ .bin/install.sh
@@ -3,21 +3,21 @@
 # https://www.atlassian.com/git/tutorials/dotfiles
 
 # add the alias
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-# add a dummy gitignore
-echo ".cfg" >> .gitignore
+# add gitignore thing
+echo ".dotfiles" >> $HOME/.gitignore
 
 # clone the repo
-git clone --bare git@github.com:tomasruud/dot.git $HOME/.cfg
+git clone --bare git@github.com:tomasruud/dot.git $HOME/.dotfiles
 
 # checkout code and try to move conflicting files automatically
-mkdir -p .config-backup && \
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .config-backup/{}
+mkdir -p $HOME/.dotfiles-backup && \
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} $HOME/.dotfiles-backup/{}
 
 # do the checkout again if it failed previously
-config checkout
+dotfiles checkout
 
 # create separate git config
 touch $HOME/.config/git/config
@@ -25,8 +25,8 @@ touch $HOME/.config/git/config
 # copy example env
 cp $HOME/.env.example $HOME/.env
 
-# make sure to use provided configignore file
-config config --local core.excludesfile $HOME/.configignore
+# make sure to configure dotfiles repo to not show untracked files
+dotfiles config --local status.showUntrackedFiles no
 
 # source the added files
 source $HOME/.bash_profile
