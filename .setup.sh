53be29a928aed51a9a4161308db89ee90e318b00
Tomas Ruud <tomasruu@gmail.com>
2020-12-17 14:14:28 +0100

    Adding docker to setup

--- .setup.sh
+++ .setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install stuff
-sudo apt install -y git vim gnome-tweaks terminator dislocker
+sudo apt install git vim gnome-tweaks terminator dislocker
 
 # install jb mono
 curl -o /tmp/jbmono.zip -L "https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip"
@@ -10,7 +10,7 @@ rm -f /tmp/jbmono.zip
 sudo fc-cache -f -v
 
 # remove ubuntu dock
-sudo apt remove gnome-shell-extension-ubuntu-dock
+sudo apt removegnome-shell-extension-ubuntu-dock
 
 # set up shortcuts
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Alt>Tab']"
@@ -22,5 +22,12 @@ gsettings set org.gnome.gedit.preferences.editor scheme "'solarized-light'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys calculator "['<Primary><Alt>k']"
 
-# install other
+# install spotify
 snap install spotify
+
+# install docker
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
