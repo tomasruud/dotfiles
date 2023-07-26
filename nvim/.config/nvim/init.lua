06dfb630ea37295803ef73573c6793c2bbb7dc55
Tomas Ruud <git@me.2ms.no>
2023-07-26 13:40:59 +0200

    Some setup for go and typescript

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,4 +2,29 @@ vim.cmd("source $HOME/.vimrc")
 
 vim.opt.clipboard = 'unnamedplus'
 
-require'lspconfig'.tsserver.setup {}
+
+lspconfig = require'lspconfig'
+util = require'lspconfig/util'
+
+lspconfig.tsserver.setup{}
+
+lspconfig.gopls.setup{
+    settings = {
+        gopls = {
+            analyses = {unusedparams = true},
+        },
+    },
+}
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
+
+vim.api.nvim_create_autocmd({'BufWritePre', 'InsertLeave'}, {
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.code_action({context = {only = {'source.organizeImports'}}, apply = true})
+    end,
+})
