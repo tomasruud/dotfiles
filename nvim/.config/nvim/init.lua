48da761f929577917a2ed9c360e616609622e0f5
Tomas Ruud <git@me.2ms.no>
2023-07-26 16:26:12 +0200

    More vim setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,4 +34,21 @@ lspconfig.gopls.setup {
 
 -- keymaps
 vim.keymap.set('n', '<Leader>/', ':noh<cr>')
-vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<Leader>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
--- vim/.vimrc
+++ vim/.vimrc
@@ -20,5 +20,7 @@ set expandtab
 
 
 let g:netrw_keepdir=0
+let g:netrw_liststyle=3
+let g:netrw_fastbrowse=2
 
 nmap <Leader>e :Explore<CR>
