5445b575027ea5da65914d90d8ef9baf48eda063
Tomas Ruud <git@me.2ms.no>
2023-07-28 13:27:08 +0200

    Improve nvim menus

--- nvim/.config/nvim/lua/2ms/core.lua
+++ nvim/.config/nvim/lua/2ms/core.lua
@@ -2,3 +2,10 @@ vim.cmd("source $HOME/.vimrc")
 
 vim.opt.clipboard = "unnamedplus"
 
+vim.opt.termiguicolors = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
